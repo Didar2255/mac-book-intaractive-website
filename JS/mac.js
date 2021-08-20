c8e003ac65e2720f20504129a91a385b8d200c5a
Didar2255 <didar.a2255@gmail.com>
2021-08-21 02:06:22 +0600

    implement total amount

--- JS/mac.js
+++ JS/mac.js
@@ -1,36 +1,65 @@
-function
 
 
 // Memory price incrase
 document.getElementById('8gb-btn').addEventListener('click', function () {
     const memory = document.getElementById('memory-cost');
     memory.innerText = '0';
+    const fixed = document.getElementById('fixed-price');
+    const fixedNumber = parseInt(fixed.innerText)
+    const totalPrice = document.getElementById('total-amount');
+    totalPrice.innerText = fixedNumber + parseInt(memory.innerText);
+
 });
 document.getElementById('16gb-btn').addEventListener('click', function () {
     const memory = document.getElementById('memory-cost');
     memory.innerText = '180';
+    const fixed = document.getElementById('fixed-price');
+    const fixedNumber = parseInt(fixed.innerText)
+    const totalPrice = document.getElementById('total-amount');
+    totalPrice.innerText = fixedNumber + parseInt(memory.innerText);
+
 });
 
 // storage cost incrase
 document.getElementById('256bg-stor-btn').addEventListener('click', function () {
     const storage = document.getElementById('storage-cost');
     storage.innerText = '0';
+    const memory = document.getElementById('memory-cost');
+    const fixed = document.getElementById('fixed-price');
+    const totalPrice = document.getElementById('total-amount');
+    totalPrice.innerText = parseInt(storage.innerText) + parseInt(memory.innerText) + parseInt(fixed.innerText);
 });
 document.getElementById('512bg-stor-btn').addEventListener('click', function () {
     const storage = document.getElementById('storage-cost');
     storage.innerText = '100';
+    const memory = document.getElementById('memory-cost');
+    const fixed = document.getElementById('fixed-price');
+    const totalPrice = document.getElementById('total-amount');
+    totalPrice.innerText = parseInt(storage.innerText) + parseInt(memory.innerText) + parseInt(fixed.innerText);
 })
 document.getElementById('1tb-stor-btn').addEventListener('click', function () {
     const storage = document.getElementById('storage-cost');
     storage.innerText = '180';
+    const memory = document.getElementById('memory-cost');
+    const fixed = document.getElementById('fixed-price');
+    const totalPrice = document.getElementById('total-amount');
+    totalPrice.innerText = parseInt(storage.innerText) + parseInt(memory.innerText) + parseInt(fixed.innerText);
 })
 
 // delivary cost 
 document.getElementById('delivery-btn1').addEventListener('click', function () {
     const delivaryCost = document.getElementById('delivery-cost');
     delivaryCost.innerText = '0';
+    const memory = document.getElementById('memory-cost');
+    const fixed = document.getElementById('fixed-price');
+    const totalPrice = document.getElementById('total-amount');
+    totalPrice.innerText = parseInt(delivaryCost.innerText) + parseInt(memory.innerText) + parseInt(fixed.innerText);
 });
 document.getElementById('delivery-btn2').addEventListener('click', function () {
     const delivaryCost = document.getElementById('delivery-cost');
     delivaryCost.innerText = '20';
+    const memory = document.getElementById('memory-cost');
+    const fixed = document.getElementById('fixed-price');
+    const totalPrice = document.getElementById('total-amount');
+    totalPrice.innerText = parseInt(delivaryCost.innerText) + parseInt(memory.innerText) + parseInt(fixed.innerText);
 })
